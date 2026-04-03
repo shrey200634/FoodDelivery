cd0ebf933d9b4da27140e37a7550ae3218a9f305
shrey200634 <dshrey60@gmail.com>
2026-04-03 23:52:34 +0530

    Add Config - RedisConfig -Kafka config - Security Config

--- Backened/order-service/src/main/java/com/foodDelivery/order_service/config/RedisConfig.java
+++ Backened/order-service/src/main/java/com/foodDelivery/order_service/config/RedisConfig.java
@@ -1,12 +1,21 @@
 package com.foodDelivery.order_service.config;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
-@RequiredArgsConstructor
-@Slf4j
+@Configuration
 public class RedisConfig {
+    @Bean
+    public ObjectMapper objectMapper(){
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        return mapper ;
+
+    }
 }
