92078967032e5a9a04a395695af3f984a69e961a
shrey200634 <dshrey60@gmail.com>
2026-05-03 14:16:21 +0530

    added new service structure

---
+++ Backened/ai-assiatant-service/src/test/java/com/foodDelivery/ai_assiatant_service/AiAssiatantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodDelivery.ai_assiatant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssiatantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
